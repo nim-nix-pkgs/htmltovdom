281a4c9e43d5c2e4c899e3da2424259c44b3ee9f
hugosenari <hugosenari@gmail.com>
2022-03-12 03:39:51 -0300

    chore: re index htmltovdom

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Karax extension to convert html in string form to embeddable Karax vdom'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."htmltovdom-main".url = "path:./main";
+  inputs."htmltovdom-main".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."htmltovdom-main".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ main/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = ''Karax extension to convert html in string form to embeddable Karax vdom'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-htmlToVdom-main.flake = false;
+  inputs.src-htmlToVdom-main.owner = "C-NERD";
+  inputs.src-htmlToVdom-main.ref   = "refs/heads/main";
+  inputs.src-htmlToVdom-main.repo  = "htmlToVdom";
+  inputs.src-htmlToVdom-main.type  = "github";
+  
+  inputs."karax".dir   = "nimpkgs/k/karax";
+  inputs."karax".owner = "riinr";
+  inputs."karax".ref   = "flake-pinning";
+  inputs."karax".repo  = "flake-nimble";
+  inputs."karax".type  = "github";
+  inputs."karax".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."karax".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-htmlToVdom-main"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-htmlToVdom-main";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
